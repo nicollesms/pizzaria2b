d815e6c6ca3607a6429ae29729ce4d7ddf39bc0e
nicollesms <nic9llenic@gmail.com>
2024-06-11 08:26:43 -0300

    retomada

---
+++ src/pages/Inicio.jsx
@@ -0,0 +1,18 @@
+import {Link, useNavigate} from 'react-router-dom'
+import logo from '../assets/react.svg'
+import '../App.css'
+ 
+const Inicio = () => {
+ 
+    return (
+        <div>
+            <h2>Título da página</h2>
+            <p>Outra tela Inicial</p>
+            <img className='img-fluid'
+            src={logo}
+                    alt='logo' ></img>
+           
+        </div>
+    )
+}
+export default Inicio
